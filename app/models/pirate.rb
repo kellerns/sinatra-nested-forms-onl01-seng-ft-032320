c9fde7b631b665756785fb3991d7ea5e99c0cc5e
Nicholas Keller <nic.keller328@gmail.com>
2020-05-07 14:16:30 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+
+  PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @ships = []
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
